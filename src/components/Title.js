073cb6123eb9be63d292a7c7525d7907840631d5
Eddy Caldas <eddycaldas@live.com>
2018-07-26 21:04:11 -0600

    init app

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+       <h1>What's the weather</h1>
+       <p>Find out the weather anywhere in the world</p>
+      </div>
+    );
+  }
+}
+
+export default Title;
